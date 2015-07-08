10cf675762911cdd57834d8045a6cd9e9c2c0bc3
Garrett Kerkkamp <garrett.kerkkamp@gmail.com>
2015-07-08 16:03:20 -0400

    small change

--- lib/dossier/adapter/active_record.rb
+++ lib/dossier/adapter/active_record.rb
@@ -15,8 +15,8 @@ module Dossier
 
       def execute(query, report_name = nil)
         # Ensure that SQL logs show name of report generating query
-        self.transaction do
-          connection.execute("SET @previous=\"\";")
+        ::ActiveRecord::Base.transaction do
+          ::ActiveRecord::Base.execute("SET @previous=\"\";")
           Result.new(connection.exec_query(*["\n#{query}", report_name].compact))
         end
 
